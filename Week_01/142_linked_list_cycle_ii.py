7477c2873ee2fb21afbb6250d3a7fa549832211f
Moz's PC <gruicn@gmail.com>
2020-08-16 23:44:07 +0800

    142 环形链表2

---
+++ Week_01/142_linked_list_cycle_ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+        if head is None or head.next is None:
+            return None
+        
+        slow = head
+        fast = head
+        
+        while True:
+            if not (fast and fast.next):
+                return None
+            
+            fast = fast.next.next
+            slow = slow.next
+
+            if fast == slow:
+                break
+        
+        new_slow = head
+        while new_slow != slow:
+            new_slow = new_slow.next
+            slow = slow.next
+        
+        return new_slow
