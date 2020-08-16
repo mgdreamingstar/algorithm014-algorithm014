5f1c4ffcf2385280e146f3570fde366c3748f0ea
Moz's PC <gruicn@gmail.com>
2020-08-16 23:38:11 +0800

    011 盛最多水的容器

---
+++ Week_01/011_container_with_most_water.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        v = 0
+        i = 0
+        j = len(height) - 1
+        while i < j:
+            area = min(height[i], height[j]) * (j - i)
+            if height[i] < height[j]:
+                i += 1
+            else:
+                j -= 1
+            
+            if v < area:
+                v = area
+        return v
