78a2f181d969958162f7ebce653d5535907f6160
Moz's PC <gruicn@gmail.com>
2020-08-16 23:40:04 +0800

    024 两两交换链表中的节点

---
+++ Week_01/024_swap_nodes_in_pairs.py
@@ -0,0 +1,12 @@
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        if not (head and head.next):
+            return head
+        
+        first = head
+        second = head.next
+        first.next = self.swapPairs(second.next)
+        second.next = first
+        
+        return second
+        
