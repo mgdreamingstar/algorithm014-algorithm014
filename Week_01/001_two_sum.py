92a3c00ea2338cb5f12d81917448d41ece51c12c
Moz's PC <gruicn@gmail.com>
2020-08-16 22:54:09 +0800

    01 两数之和

---
+++ Week_01/001_two_sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+        1st Solution (slow):
+        for i in range(len(nums)):
+            a = nums[i]
+            b = target - a
+            for j in range(i + 1, len(nums)):
+                if nums[j] == b:
+                    return [i, j]
+                    
+        2nd Solution (fast):
+        """
+        hash_nums = {}
+        for ind, num in enumerate(nums):
+            hash_nums[num] = ind
+        for ind, num in enumerate(nums):
+            if target - num in hash_nums and ind != hash_nums[target - num]:
+                return [ind, hash_nums[target - num]] 
