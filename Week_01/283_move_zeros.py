31a126d4975ab63bf05d9faa13a8f943fb61e1f3
Moz's PC <gruicn@gmail.com>
2020-08-16 23:11:07 +0800

    283 移动零

---
+++ Week_01/283_move_zeros.py
@@ -0,0 +1,18 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        j = 0 
+        i = 1
+        while i < len(nums):
+            if nums[i] == 0 and nums[j] == 0:
+                i += 1
+                continue
+            
+            if nums[i] != 0 and nums[j] == 0:
+                nums[j], nums[i] = nums[i], nums[j]
+            
+            i += 1
+            j += 1
+            
