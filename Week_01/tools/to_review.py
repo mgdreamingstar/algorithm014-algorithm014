c52a0e7493bc7027d9856f840c085b6729c926ac
Moz's PC <gruicn@gmail.com>
2020-08-19 14:25:03 +0800

    以日期为分隔，获取任务

--- Week_01/tools/to_review.py
+++ Week_01/tools/to_review.py
@@ -1,6 +1,7 @@
 import re
 from re import template
 from pprint import pprint
+from datetime import datetime
 
 
 def get_status():
@@ -24,6 +25,30 @@ def get_status():
     return sorted(status_dict.items(), key=lambda x: x[1], reverse=True)
 
 
+def get_tasks_by_day():
+    with open(
+        r"D:\mozli\Documents\GitHub\Python_Repo\LeetCode\leetcode.taskpaper", "r", encoding="utf-8"
+    ) as f:
+        data = f.readlines()
+
+    # today_str = datetime.today().strftime("%Y-%m-%d")
+    # last_date = today_str
+    tasks_dict = {}
+    last_date = ""
+
+    for item in data:
+        if re.match(r"\d{4}-\d{2}-\d{2}", item):
+            date = re.match(r"\d{4}-\d{2}-\d{2}", item).group()
+            last_date = date
+            tasks_dict[date] = []
+        elif item != r"\n" and not re.match(r"^[ \n]*$", item):
+            task = item.split("#")[0]
+            task_formated = task.lstrip()
+            tasks_dict[last_date].append(task_formated)
+
+    return tasks_dict
+
+
 def is_all_done():
     with open(
         r"D:\mozli\Documents\GitHub\Python_Repo\LeetCode\leetcode.taskpaper", "r", encoding="utf-8"
@@ -34,10 +59,14 @@ def is_all_done():
         return False
     elif data[-1][:5] == "  [x]":
         return True
+    else:
+        return "taskpaper not in right format."
 
 
 if __name__ == "__main__":
     pprint(get_status())
     print("\nStatus:")
     print("  all done." if is_all_done() else "  not all done.")
+    print()
+    pprint(get_tasks_by_day())
 
