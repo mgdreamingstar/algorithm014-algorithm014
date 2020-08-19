9609a4d2155aaaa9d634884b9a0ea22dcca1c10b
Moz's PC <gruicn@gmail.com>
2020-08-19 13:45:18 +0800

    检查是否都完成的函数。

--- Week_01/tools/to_review.py
+++ Week_01/tools/to_review.py
@@ -8,19 +8,36 @@ def get_status():
     get leetcode quizs' status.
     """
 
-    with open(r"D:\mozli\Documents\GitHub\Python_Repo\LeetCode\leetcode.taskpaper", "r", encoding='utf-8') as f:
+    with open(
+        r"D:\mozli\Documents\GitHub\Python_Repo\LeetCode\leetcode.taskpaper", "r", encoding="utf-8"
+    ) as f:
         data = f.read()
         quiz_status = re.findall(r"\d{3}.*#\d", data)
 
     status_dict = {}
     for item in quiz_status:
-        temp_list = item.split('#')
+        temp_list = item.split("#")
         already = status_dict.get(temp_list[0], 0)
         if already < int(temp_list[1]):
             status_dict[temp_list[0]] = int(temp_list[1])
-        
+
     return sorted(status_dict.items(), key=lambda x: x[1], reverse=True)
+
+
+def is_all_done():
+    with open(
+        r"D:\mozli\Documents\GitHub\Python_Repo\LeetCode\leetcode.taskpaper", "r", encoding="utf-8"
+    ) as f:
+        data = f.readlines()
+
+    if data[-1][:5] == "  [ ]":
+        return False
+    elif data[-1][:5] == "  [x]":
+        return True
+
 
 if __name__ == "__main__":
     pprint(get_status())
+    print("\nStatus:")
+    print("  all done." if is_all_done() else "  not all done.")
 
