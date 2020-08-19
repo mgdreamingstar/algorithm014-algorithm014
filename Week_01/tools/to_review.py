31f24af45df29930182c79d66e5b12866271da2b
Moz's PC <gruicn@gmail.com>
2020-08-19 16:44:38 +0800

    获取需要复习的题目

--- Week_01/tools/to_review.py
+++ Week_01/tools/to_review.py
@@ -1,7 +1,7 @@
 import re
 from re import template
 from pprint import pprint
-from datetime import datetime
+import datetime
 
 
 def get_status():
@@ -43,7 +43,7 @@ def get_tasks_by_day():
             tasks_dict[date] = []
         elif item != r"\n" and not re.match(r"^[ \n]*$", item):
             task = item.split("#")[0]
-            task_formated = task.lstrip()
+            task_formated = task.lstrip()[4:]
             tasks_dict[last_date].append(task_formated)
 
     return tasks_dict
@@ -63,10 +63,68 @@ def is_all_done():
         return "taskpaper not in right format."
 
 
+def new_review():
+    """
+    add new todo
+    """
+    if is_all_done():
+        today_str = datetime.datetime.today().strftime("%Y-%m-%d")
+        one_day = datetime.timedelta(days=1)
+        task_dict = get_tasks_by_day()
+
+        dates_strlist = task_dict.keys()
+        tasks_add_raw = []
+        tasks_add_dict = {}
+        for key in list(dates_strlist):
+            today = datetime.datetime(
+                datetime.datetime.today().year,
+                datetime.datetime.today().month,
+                datetime.datetime.today().day,
+            )
+            date = datetime.datetime.strptime(key, "%Y-%m-%d")
+
+            """
+            review rule (by day):
+                Day 1-3 +
+                Day 4   x
+                Day 5   +
+                Day 6-7 x
+                Day 8   +
+                After repeat 5 times:
+                    review every week
+            """
+            if today - date <= one_day * 3:
+                tasks_add_raw.extend(task_dict[key])
+            elif today - date == one_day * 4:
+                continue
+            elif today - date == one_day * 5:
+                tasks_add_raw.extend(task_dict[key])
+            elif one_day * 5 < today - date <= one_day * 7:
+                continue
+            elif today - date == one_day * 8:
+                tasks_add_raw.extend(task_dict[key])
+            elif (today - date) % 7 == 0:
+                tasks_add_raw.extend(task_dict[key])
+
+        status = dict(get_status())
+        for item in tasks_add_raw:
+            repeat = status[item]
+            if int(repeat) < 8:
+                tasks_add_dict[item] = status[item] + 1
+            else:
+                continue
+
+        return tasks_add_dict
+
+    else:
+        return "Please complete all tasks."
+
+
 if __name__ == "__main__":
-    pprint(get_status())
-    print("\nStatus:")
-    print("  all done." if is_all_done() else "  not all done.")
-    print()
-    pprint(get_tasks_by_day())
+    pprint(dict(get_status()))
+    # print("\nStatus:")
+    # print("  all done." if is_all_done() else "  not all done.")
+    # print()
+    # pprint(get_tasks_by_day())
+    pprint(new_review())
 
