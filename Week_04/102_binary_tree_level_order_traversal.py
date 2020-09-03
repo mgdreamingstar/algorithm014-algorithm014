b0dfe0bc443f3ce409cf088477b7b3185d4ca1b0
Moz's PC <gruicn@gmail.com>
2020-09-03 16:57:10 +0800

    102 二叉树层序遍历

---
+++ Week_04/102_binary_tree_level_order_traversal.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+        ans, queue = [[root.val]], [[root]]
+        while queue:
+            now = queue.pop(0)
+            level = []
+            for node in now:
+                if node.left:
+                    level.append(node.left)
+                if node.right:
+                    level.append(node.right)
+            if level != []:
+                ans.append([node.val for node in level])
+                queue.append(level)
+        return ans
+            
