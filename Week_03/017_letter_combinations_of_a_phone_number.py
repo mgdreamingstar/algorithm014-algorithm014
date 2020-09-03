e9909df4df4cbd87dfa98af99555980c588fa981
Moz's PC <gruicn@gmail.com>
2020-09-03 08:22:56 +0800

    formatting

--- Week_03/017_letter_combinations_of_a_phone_number.py
+++ Week_03/017_letter_combinations_of_a_phone_number.py
@@ -1,22 +1,24 @@
 class Solution(object):
-	def letterCombinations(self, digits):
-		"""
-		:type digits: str
-		:rtype: List[str]
-		"""
-		if not digits:
-			return []
-		d = [" ","*","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"]
-		res = []
-		
-		def dfs(tmp,index):
-			if index==len(digits):
-				res.append(tmp)
-				return
-			c = digits[index]
-			letters = d[int(c)]
-			
-			for i in letters:
-				dfs(tmp+i,index+1)
-		dfs("",0)
-		return res
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        if not digits:
+            return []
+        d = [" ", "*", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
+        res = []
+
+        def dfs(tmp, index):
+            if index == len(digits):
+                res.append(tmp)
+                return
+            c = digits[index]
+            letters = d[int(c)]
+
+            for i in letters:
+                dfs(tmp + i, index + 1)
+
+        dfs("", 0)
+        return res
+
