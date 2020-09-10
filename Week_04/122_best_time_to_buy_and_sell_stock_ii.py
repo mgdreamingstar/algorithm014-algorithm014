a89cd5c1836c938f9c8beeb68bf9f5d3720b7009
Moz's PC <gruicn@gmail.com>
2020-09-10 20:41:24 +0800

    122 买卖股票的最佳时机 ii

---
+++ Week_04/122_best_time_to_buy_and_sell_stock_ii.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        ans = 0
+        for i in range(len(prices) - 1):
+            diff = prices[i + 1] - prices[i]
+            if diff > 0:
+                ans += diff
+        return ans
