5602815b395d02d39d5c7427972202605f18227c
Moz's PC <gruicn@gmail.com>
2020-10-03 11:04:13 +0800

    0552 学生出勤记录 II

---
+++ Week_06/0552_student_attendance_record_ii.py
@@ -0,0 +1,11 @@
+import operator
+
+
+class Solution:
+    def checkRecord(self, n: int) -> int:
+        if n < 2:
+            return n * 3
+        nums = [1]
+        while len(nums) <= n + 1:
+            nums.append(sum(nums[-3:]) % 1000000007)
+        return sum(map(operator.mul, nums[:-1], nums[:0:-1])) % 1000000007
