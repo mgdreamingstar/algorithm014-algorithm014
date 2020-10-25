9792c407af48bef308163c2b4157fc3094e86b1c
Moz's PC <gruicn@gmail.com>
2020-10-25 16:02:09 +0800

    复习

--- Week_02/429_n_ary_tree_level_order_traversal.py
+++ Week_02/429_n_ary_tree_level_order_traversal.py
@@ -7,7 +7,11 @@ class Node:
 
 def levelOrder(self, root: Node) -> List[List[int]]:
     def traverse_node(node, level):
-        if len(result) == level:
+        """
+        这是深度优先，
+        当节点没有 children 时，开始回退，然后横向寻找
+        """
+        if len(result) == level:  # 每次新进入一层，创建
             result.append([])
         result[level].append(node.val)
         for child in node.children:
