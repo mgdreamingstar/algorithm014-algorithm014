2534e9b8d31d82d2020baeb7b751a5013a65d915
Moz's PC <gruicn@gmail.com>
2020-10-30 21:47:04 +0800

    0127 单词接龙（双向 BFS）

---
+++ Week_07/0127_word_ladder_2.py
@@ -0,0 +1,34 @@
+import string
+from typing import List
+
+
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if endWord not in wordList:
+            return 0
+        front = {beginWord}
+        back = {endWord}
+        dist = 1  # 走的步数
+        wordList = set(wordList)
+        word_len = len(beginWord)
+
+        # BFS start here
+        while front and back:
+            dist += 1
+            next_front = set()
+            for word in front:
+                for i in range(word_len):
+                    for c in string.ascii_lowercase:
+                        if c != word[i]:
+                            new_word = word[:i] + c + word[i + 1 :]
+                            if new_word in back:
+                                return dist
+
+                            if new_word in wordList:
+                                next_front.add(new_word)
+                                wordList.remove(new_word)
+            front = next_front
+
+            if len(back) < len(front):
+                front, back = back, front
+        return 0
