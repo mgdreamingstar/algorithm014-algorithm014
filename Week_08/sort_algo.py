a85f7a0ab49116adf8a818fa6d5cb0f0c5409b4e
Moz's PC <gruicn@gmail.com>
2020-11-16 14:21:38 +0800

    堆排序

--- Week_08/sort_algo.py
+++ Week_08/sort_algo.py
@@ -99,6 +99,39 @@ def merge_sort(nums, left, right):
     return nums
 
 
+def heap_sort(nums):
+    def heapify(parent_index, length, nums):
+        temp = nums[parent_index]  # 保存父节点的值
+        child_index = 2 * parent_index + 1
+        while child_index < length:
+            if child_index + 1 < length and nums[child_index + 1] > nums[child_index]:
+                child_index = child_index + 1
+            if temp > nums[child_index]:  # 大顶堆，已满足条件（父 > 子）
+                break
+            # 继续向下
+            nums[parent_index] = nums[child_index]
+            parent_index = child_index
+            child_index = 2 * parent_index + 1
+        # 最终将初始父节点值放下
+        nums[parent_index] = temp
+
+    """
+    遍历所有子节点的父节点，最终获取到了最大值，放在 index = 0 位置
+    不从最后遍历，是因为会重复遍历
+    """
+    for i in range((len(nums) - 2) // 2, -1, -1):
+        heapify(i, len(nums), nums)
+    
+    """
+    将最大的元素放在最后（j 的位置）
+    j 不断前移，表明其之后的位置已排序完成
+    """
+    for j in range(len(nums) - 1, 0, -1):
+        nums[j], nums[0] = nums[0], nums[j]
+        heapify(0, j, nums)
+    return nums
+
+
 if __name__ == "__main__":
     nums = [1, 3, 4, 5, 2]
     # print("bubble sort:", bubble_sort(nums))
@@ -106,5 +139,6 @@ if __name__ == "__main__":
     # print("insertion sort:", insertion_sort(nums))
     # print("shell sort:", shell_sort(nums))
     # print("quick sort:", quick_sort(nums, 0, len(nums) - 1))
-    print("merge sort:", merge_sort(nums, 0, len(nums) - 1))
+    # print("merge sort:", merge_sort(nums, 0, len(nums) - 1))
+    print("heap sort:", heap_sort([2, 3, 4, 5, 1, 6, 7, 9, 8, 10, 11]))
 
